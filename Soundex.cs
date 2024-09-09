15b94ed0e39ac225e539b6d7813c6389f13be8e6
17trupti <167959684+17trupti@users.noreply.github.com>
2024-09-09 14:51:01 +0530

    UpdateSoundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -1,102 +1,71 @@
 using System;
 using System.Text;
+using System.Collections.Generic;
 
 public class Soundex
 {
-    private static readonly string VowelsAndIgnored = "AEIOUYHW";
-    private static readonly string[] Codes = {
-        "BFPV",  // 1
-        "CGJKQSXZ",  // 2
-        "DT",  // 3
-        "L",  // 4
-        "MN",  // 5
-        "R"   // 6
+    private static readonly Dictionary<char, char> soundexMapping = new Dictionary<char, char>
+    {
+        { 'B', '1' }, { 'F', '1' }, { 'P', '1' }, { 'V', '1' },
+        { 'C', '2' }, { 'G', '2' }, { 'J', '2' }, { 'K', '2' },
+        { 'Q', '2' }, { 'S', '2' }, { 'X', '2' }, { 'Z', '2' },
+        { 'D', '3' }, { 'T', '3' },
+        { 'L', '4' },
+        { 'M', '5' }, { 'N', '5' },
+        { 'R', '6' }
     };
 
     public static string GenerateSoundex(string name)
     {
-        if (IsInvalidName(name)) return string.Empty;
-
-        StringBuilder soundex = BuildSoundexBase(name);
-
-        ProcessRemainingLetters(name, soundex);
-
-        PadWithZeros(soundex);
-
-        return soundex.ToString();
-    }
-
-    private static bool IsInvalidName(string name) => string.IsNullOrEmpty(name);
-
-    private static StringBuilder BuildSoundexBase(string name)
-    {
-        StringBuilder soundex = new StringBuilder();
-        soundex.Append(char.ToUpper(name[0]));  // Retain the first letter
-        return soundex;
-    }
-
-    private static void ProcessRemainingLetters(string name, StringBuilder soundex)
-    {
-        int codeCount = 0;
-        char prevCode = GetSoundexCode(name[0]);
-        
-        ProcessLettersInName(name, ref codeCount, ref prevCode, soundex);
-    }
-
-    private static void ProcessLettersInName(string name, ref int codeCount, ref char prevCode, StringBuilder soundex)
-    {
-        int length = name.Length;
-        for (int i = 1; i < length && codeCount < 3; i++)
+        if (string.IsNullOrEmpty(name))
         {
-            if (!ShouldSkipCurrentLetter(name, i, prevCode))
+            return string.Empty;
+        }
+
+        string processedName = RemoveUnnecessaryCharacters(name);
+        StringBuilder soundex = new StringBuilder();
+        soundex.Append(char.ToUpper(name[0]));
+
+        char prevCode = GetSoundexCode(name[0]);
+
+        for (int i = 1; i < processedName.Length && soundex.Length < 4; i++)
+        {
+            char code = GetSoundexCode(processedName[i]);
+            if (code != '0' && code != prevCode)
             {
-                AppendCurrentCode(name, soundex, ref prevCode, ref codeCount, i);
+                soundex.Append(code);
+                prevCode = code;
             }
         }
-    }
-    
-    private static void AppendCurrentCode(string name, StringBuilder soundex, ref char prevCode, ref int codeCount, int index)
-    {
-        char currentCode = GetSoundexCode(name[index]);
-        soundex.Append(currentCode);
-        prevCode = currentCode;
-        codeCount++;
-    }
-    
-    private static bool ShouldSkipCurrentLetter(string name, int index, char prevCode)
-    {
-        char currentChar = char.ToUpper(name[index]);
-        if (IsVowelOrIgnored(currentChar)) return true;
-        
-        char currentCode = GetSoundexCode(currentChar);
-        return currentCode == prevCode || IsHOrWSeparated(name, index);
+
+        return PadSoundexCode(soundex);
     }
 
-    private static bool IsVowelOrIgnored(char c) => VowelsAndIgnored.Contains(c);
+    private static string RemoveUnnecessaryCharacters(string input)
+    {
+        StringBuilder result = new StringBuilder();
+        foreach (char c in input)
+        {
+            if (!"AEIOUYHW".Contains(char.ToUpper(c)))
+            {
+                result.Append(c);
+            }
+        }
+        return result.ToString();
+    }
 
-    private static void PadWithZeros(StringBuilder soundex)
+    private static char GetSoundexCode(char c)
+    {
+        c = char.ToUpper(c);
+        return soundexMapping.ContainsKey(c) ? soundexMapping[c] : '0';
+    }
+
+    private static string PadSoundexCode(StringBuilder soundex)
     {
         while (soundex.Length < 4)
         {
             soundex.Append('0');
         }
-    }
-
-    private static char GetSoundexCode(char c)
-    {
-        char upperChar = char.ToUpper(c);
-        for (int i = 0; i < Codes.Length; i++)
-        {
-            if (Codes[i].Contains(upperChar))
-            {
-                return (char)('1' + i);
-            }
-        }
-        return '0';
-    }
-
-    private static bool IsHOrWSeparated(string name, int index)
-    {
-        return index > 1 && (name[index - 1] == 'H' || name[index - 1] == 'W');
+        return soundex.Length > 4 ? soundex.ToString().Substring(0, 4) : soundex.ToString();
     }
 }
