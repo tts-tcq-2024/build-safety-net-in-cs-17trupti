e25e92aab2820da707831e6967517e7e0290c076
17trupti <167959684+17trupti@users.noreply.github.com>
2024-09-23 16:08:49 +0530

    Update Soundex.Tests.cs

--- Soundex.Tests.cs
+++ Soundex.Tests.cs
@@ -2,40 +2,66 @@ using Xunit;
 
 public class SoundexTests
 {
+
     [Fact]
-    public void GenerateSoundex_ShouldReturnSoundexCode_WhenValidNameIsProvided()
+    public void GenerateSoundex_ShouldReturnEmptySoundexCode_WhenInputIsEmpty()
+    {
+        Assert.Equal(string.Empty, Soundex.GenerateSoundex(""));
+    }
+
+    [Fact]
+    public void GenerateSoundex_shouldReturnValidCode_WhenInputIsSingleNon-MappedLetter()
+    {
+        var name = "A";
+        var result = Soundex.GenerateSoundex(name);
+
+        Assert.Equal("A000", result);
+    }
+
+    [Fact]
+    public void GenerateSoundex_shouldReturnValidCode_WhenInputIsSingleMappedLetter()
+    {
+        var name = "B";
+        var result = Soundex.GenerateSoundex(name);
+
+        Assert.Equal("B000", result);
+    }
+
+    [Fact]
+    public void GenerateSoundex_shouldReturnValidCode_WhenInputWithAllLettersOfSameSoundexGroup()
+    {
+        var name = "BB";
+        var result = Soundex.GenerateSoundex(name);
+
+        Assert.Equal("B100", result);
+    }
+    
+    [Fact]
+    public void GenerateSoundex_shouldReturnValidCode_WhenInputWithMultipleSoundexGroups()
     {
         var name1 = "Smith";
         var name2 = "Robert";
         var name3 = "Harris";
+        var name4 = "Robert";
 
         var result1 = Soundex.GenerateSoundex(name1);
         var result2 = Soundex.GenerateSoundex(name2);
         var result3 = Soundex.GenerateSoundex(name3);
+        var result4 = Soundex.GenerateSoundex(name4);
 
         Assert.Equal("S530", result1);
         Assert.Equal("R163", result2);
         Assert.Equal("H620", result3);
+        Assert.Equal("R163", result4);
     }
 
     [Fact]
-    public void GenerateSoundex_ShouldPadWithZeros_WhenNameHasFewerThanFourDistinctCodes()
+    public void GenerateSoundex_shouldReturnValidCode_WhenInputWithConsecutiveLettersFromSameSoundexGroups()
     {
-        var name = "Abe";
-
+        var name = "Rupert";
         var result = Soundex.GenerateSoundex(name);
 
-        Assert.Equal("A100", result);
-    }
-
-    [Fact]
-    public void GenerateSoundex_ShouldHandleRepeatedCharactersCorrectly()
-    {
-        var name = "Bobby";
-
-        var result = Soundex.GenerateSoundex(name);
-
-        Assert.Equal("B000", result);
+        Assert.Equal("R163", result);
     }
 
     [Fact]
@@ -52,6 +78,24 @@ public class SoundexTests
     }
 
     [Fact]
+    public void GenerateSoundex_shouldReturnValidCode_WhenInputWithLowerCaseLetters()
+    {
+        var name = "rubin";
+        var result = Soundex.GenerateSoundex(name);
+
+        Assert.Equal("R150", result);
+    }
+
+    [Fact]
+    public void GenerateSoundex_shouldHandelInputWithCharactersOfSameSoundexGroupButSeperatedByVowel()
+    {
+        var name = "Bakes";
+        var result = Soundex.GenerateSoundex(name);
+
+        Assert.Equal("B220", result);
+    }
+
+    [Fact]
     public void GenerateSoundex_ShouldHandleLongNamesByTruncatingToFourCharacters()
     {
         var name = "Washington";
@@ -62,13 +106,33 @@ public class SoundexTests
     }
 
     [Fact]
-    public void GenerateSoundex_ShouldHandleSingleCharacterNames()
+    public void GenerateSoundex_ShouldPadWithZeros_WhenNameHasFewerThanFourDistinctCodes()
     {
-        var name = "Q";
+        var name = "Abe";
 
         var result = Soundex.GenerateSoundex(name);
 
-        Assert.Equal("Q000", result);
+        Assert.Equal("A100", result);
+    }
+
+    [Fact]
+    public void GenerateSoundex_ShouldPadWithZeros_WhenInputWithLettersResultingWithSoundexLesserThanFour()
+    {
+        var name = "Omar";
+
+        var result = Soundex.GenerateSoundex(name);
+
+        Assert.Equal("O560", result);
+    }
+
+    [Fact]
+    public void GenerateSoundex_shouldReturnValidCode_WhenInputContainingRepeatedLettersWithSoundexCode()
+    {
+        var name = "Tennessee";
+
+        var result = Soundex.GenerateSoundex(name);
+
+        Assert.Equal("T520", result);
     }
     
 }
